1ceaec2e5b413310f452945a1920493ef2a3ab87
domklyve <73136389+domklyve@users.noreply.github.com>
2026-09-09 08:28:48 -0700

    Update service-worker.js bumped version

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'ehs-schedule-v14';
+const CACHE_NAME = 'ehs-schedule-v16';
 const CORE_HTML = ['./', './index.html'];
 const ASSETS = [
   './manifest.json',
